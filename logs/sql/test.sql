43c753ec6cb874451f8493876700c58bcce8df2d
Chris Bandy <bandy.chris@gmail.com>
2022-10-21 22:29:42 -0500

    Remove early attempts

--- logs/sql/test.sql
+++
@@ -1,7 +0,0 @@
--- vim: set expandtab shiftwidth=0 syntax=pgsql tabstop=2 :
-
--- TODO why
-CREATE EXTENSION file_fdw;
-CREATE SERVER otel_lines FOREIGN DATA WRAPPER file_fdw;
-CREATE FOREIGN TABLE otel_lines (line text)
-       SERVER otel_lines OPTIONS (filename '/tmp/pg-otel.txt');
